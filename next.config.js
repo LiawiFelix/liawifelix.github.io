66b7ce66ebf0bb6000a02086a590a0b21379c705
Felix Liawi <felixliawi@gmail.com>
2023-03-04 14:57:55 +0800

    Remove base path

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = undefined;
-let basePath = "";
-
-if (isGithubActions) {
-  assetPrefix = `/liawifelix.github.io/`;
-  basePath = `/liawifelix.github.io`;
-}
-
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
     unoptimized: true,
   },
